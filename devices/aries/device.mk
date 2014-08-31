6a18c5a23a162fb2bd190e71464cfa87cee7fcbd
M1cha <sigmaepsilon92@gmail.com>
2014-08-31 15:12:46 +0200

    devices: aries: use new LK makefile

--- devices/aries/device.mk
+++ devices/aries/device.mk
@@ -1,7 +1,7 @@
 GRUB_LOADING_ADDRESS = 0x8AF00000
 GRUB_FONT_SIZE = 32
 GRUB_BOOT_PARTITION = "storage"
-LK_TARGET_NAME = msm8960
+LK_TARGET_NAME = msm8960_aries
 
 build: grub_boot_fs lk
 .PHONY : build
