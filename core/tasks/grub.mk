01ed53fcab1fe425aafb191b7149020afaa71ad8
M1cha <sigmaepsilon92@gmail.com>
2014-09-02 22:27:19 +0200

    core: tasks: grub: use git to remove ignored files

--- core/tasks/grub.mk
+++ core/tasks/grub.mk
@@ -73,7 +73,7 @@ grub_clean:
 
 grub_distclean:
 	if [ -f $(GRUB_OUT)/Makefile ]; then $(MAKE) -C $(GRUB_OUT) distclean; fi
-	cat $(GRUB_DIR)/.gitignore | xargs -I{} rm -Rf $(GRUB_DIR)/{}
+	git -C $(GRUB_DIR)/ clean -dfX
 	rm -Rf $(GRUB_OUT)/*
 	rm -Rf $(GRUB_TARGET_OUT)/*
 .PHONY : grub_distclean
